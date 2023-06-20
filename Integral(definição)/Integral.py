a32f4aed6b38fb85b4e1a1dd127d61ea6ac4d18f
Celtico(Alan) <alanmesquita2022@gmail.com>
2023-06-20 16:55:07 -0300

    Integral trabalhada

---
+++ Integral(definição)/Integral.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 20 16:33:40 2023
+
+@author: alanm
+"""
+import math as mth
+
+
+def Integral(x):
+    return mth.e ** -x + mth.cos(3 * x) - x ** 4
+
+#intervalos
+
+a = 1
+b = 3
+
+# subintervalos
+
+n = [10, 15, 20]
